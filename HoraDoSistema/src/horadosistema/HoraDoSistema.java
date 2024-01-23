f47605f54a5c3945368191854f075041bbb25a90
Wayne Junior <waynebarbosa10@hotmail.com>
2024-01-23 14:50:09 -0300

    Hora do Sistema
    Projeto usado para aprender a pegar a data do sistema utilizando o Java

---
+++ HoraDoSistema/src/horadosistema/HoraDoSistema.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package horadosistema;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ *
+ * @author wayne
+ */
+public class HoraDoSistema {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Date relogio = new Date();
+        System.out.println("A hora do sistema é");
+        System.out.println(relogio.toString());
+    }
+    
+}
